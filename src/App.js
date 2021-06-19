fcef5e4b52fe5dbfdc63f255e4c05dabbc5f24f7
andrea ilieschi <andrea.ilieschi@gmail.com>
2021-06-19 08:44:47 +0200

    Iteration 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  // remember we'll have 4 app states
+ // foods: initialFoods
+  // searchValue -> input tendré que hacer un filter
+  // todaysFood
+  // showForm: false/true
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from 'react';
+import './App.css';
+import foods from './foods.json';
+
+import FoodBox from './components/FoodBox'
+import Form from './components/Form'
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     quantity: 1,
+  //   }
+  // }
+  render () {
+    return(
+      <>
+      <h1>Iron Nutrition</h1>
+      <Form />
+     
+       
+       {foods.map((food, index) => {
+         return (
+           <FoodBox 
+             name={food.name}
+             image={food.image}
+             calories={food.calories}
+           />
+         )
+       })}
+     </>
+    )
+  }
 }
 
 export default App;
+
+
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+    render() {
+        return (
+            <div>This is my Form Component</div>
+        )
+    }
+}
+
+export default Form;
